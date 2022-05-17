8c4cc135b94dc7bf93093d41ac7e347a960f83c9
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2022-05-17 22:18:37 +0200

    refactor e implementar sesiones

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoDBSession = require('connect-mongodb-session')(session);
 const bodyParser = require('body-parser');
 const app = express();
 
-// BODYPARSER configurar para formularios y json
+/**
+ * * BODYPARSER parsea body para acceso a req.body ------------
+ */
+//  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-// variables de entorno
+/**
+ * * Variables de entorno -------------------------------------
+ */
 require('dotenv').config();
+
+/**
+ * * MongoDB --------------------------------------------------
+ */
+
 const puerto = process.env.PORT || 5000;
 
-// conexión BBDD
-const mongoose = require('mongoose');
+// conectar BBDD
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.8vyfx.mongodb.net/carpop?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log("conectado a BBDD");
+})
+.catch(e=>{
+    console.error("error conexion a BBDD: "+e);
+});
+// configuracion para guardar las sesiones en la BBDD
+const store = new MongoDBSession({
+    uri: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.8vyfx.mongodb.net/carpop?retryWrites=true&w=majority`,
+    collection: "sesiones",
+
+});
+
+// middleware que crea en el objeto request una session y lo guarda en la BBDD
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false, // no crear nueva sesion por cada request
+    saveUninitialized: true,
+    store: store
+}))
+
+/**
+ * * EJS y Routers --------------------------------------------
+ */
 
 // establecer ruta plantillas ejs y motor de render
 app.set('view engine', 'ejs');
@@ -31,16 +68,8 @@ app.use((req, res, next) => {
 })
 
 /**
- * *Mongoose
+ * * Iniciar servidor -----------------------------------------
  */
-mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.8vyfx.mongodb.net/carpop?retryWrites=true&w=majority`)
-.then(()=>{
-    console.log("conectado a BBDD");
-})
-.catch(e=>{
-    console.error("error conexion a BBDD: "+e);
-});
-
 app.listen(puerto, () => {
 
     console.log("escuchando");
