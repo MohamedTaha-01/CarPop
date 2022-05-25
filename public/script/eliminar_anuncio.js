0401ee571a7f66f2b7fbd753bbde3eaa8bf229b8
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2022-05-25 21:19:51 +0200

    separo script eliminar anuncio en 2 scripts

---
+++ public/script/eliminar_anuncio.js
@@ -0,0 +1,24 @@
+import {crearModal} from './modal.js';
+
+// boton eliminar anuncio
+let bEliminarAn = document.querySelectorAll('.b-eliminar-anuncio');
+bEliminarAn.forEach(boton => {
+    
+    boton.addEventListener("click", async() =>{
+        let idEliminarAn = boton.dataset.id;
+        try {
+            const res = await fetch(`/admin/anuncios/eliminar/${idEliminarAn}`, {method: 'DELETE'});
+            const resjson = await res.json();
+            if (resjson.eliminado) {
+                crearModal(resjson.mensaje, false, 3000, true);
+                setTimeout(() => {
+                    window.location.href = "/anuncios";
+                }, 1500);
+            } else {
+                crearModal("Error", resjson.mensaje, 5000, true);
+            }
+        } catch (error) {
+            crearModal("Error", error, 3000, true);
+        }
+    });
+});
