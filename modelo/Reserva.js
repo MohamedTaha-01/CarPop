5448ca7e1eb32ced9429af7b4eed317e2ad3666b
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2022-05-29 21:46:40 +0200

    añadir crear reservas

---
+++ modelo/Reserva.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reservaSchema = new Schema({
+    id_anuncio: String,
+    id_usuario: String,
+    fecha1: Date,
+    fecha2: Date
+});
+
+const Reserva = mongoose.model('Reserva', reservaSchema);
+
+module.exports = Reserva;
